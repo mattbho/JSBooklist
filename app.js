1b2f22575d79496f774678dc4b2ece0f4dad6334
Matt Ho <mtthwbh@gmail.com>
2018-12-25 14:37:46 -0800

    Books can be deleted

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ class UI {
     list.appendChild(row);
   }
 
+  static deleteBook(el){
+    if(el.classList.contains('delete')){
+      el.parentElement.parentElement.remove();
+    }
+  }
+
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
@@ -75,3 +81,6 @@ document.querySelector('#book-form').addEventListener('submit', e =>{
   UI.clearFields();
 })
 //Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', e => {
+  UI.deleteBook(e.target);
+})
