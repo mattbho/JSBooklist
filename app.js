41d757a07e706579b85dfbc4b2b7d37e3495f290
Matt Ho <mtthwbh@gmail.com>
2018-12-25 14:22:03 -0800

    Books can be added to screen

---
+++ app.js
@@ -0,0 +1,71 @@
+//Book Class : Represent a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+//UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: 'Book One',
+        author: 'John Doe',
+        isbn: '123135664'
+      },
+      {
+        title: 'Book Two',
+        author: 'Bobby Mognahan',
+        isbn: '123135859'
+      }
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach(book => UI.addBookToList(book))
+  }
+
+  static addBookToList(book){
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class = "btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
+
+//Store Class: Handle Storage
+
+//Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+//Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', e =>{
+  //Prevent actual submit
+  e.preventDefault();
+  
+  //Get Form Values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  //Instantiate Book
+  const book = new Book(title, author, isbn);
+
+  //Add Book to UI
+  UI.addBookToList(book);
+
+  //Clear fields
+ 
+})
+//Event: Remove a Book
